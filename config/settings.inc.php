3d9792b91dcb29db5ba75d60e9badebde11ebac7
lorafbm <gsl_billdind@mail.ru>
2017-10-03 09:32:12 +0300

    initial commit

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '1');
+define('_COOKIE_KEY_', 'b5uQ1XadI2YTHzbJYwD4NhITQMD9PKzfIkaSsCCa1Eki6o4JLRjxOrt1');
+define('_COOKIE_IV_', 'PvTi9ubp');
+define('_PS_CREATION_DATE_', '2017-09-14');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.16');
+define('_RIJNDAEL_KEY_', 'nvt3E6ijLXkcSI4r6LfpyA24QAnX9oOi');
+define('_RIJNDAEL_IV_', 'br8LyzKOhY58iKmw9PXeOw==');
